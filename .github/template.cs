dc67bab85517c5f1c2a63637f60388421651bb61
kingturle559 <s11580859@students.duvalschools.org>
2024-09-04 14:49:30 -0400

    Update template.cs

--- .github/template.cs
+++ .github/template.cs
@@ -1,6 +1,22 @@
 using System;
 class Template {
   static void Main() {
-  
+    
+ int PlayerLevel = 3;
+ int PlayerScore = Convert.ToInt32(Console.ReadLine());
+  if ( PlayerScore <= 10000)
+  {
+    PlayerLevel--;
+  } 
+  else if (PlayerScore <= 10001)
+  {
+    PlayerLevel++;
   }
+  else if (PlayerScore > 100000)
+  {
+    PlayerLevel+=2;
+  }
+    Console.WriteLine (PlayerLevel);
+  }
+  
 }
