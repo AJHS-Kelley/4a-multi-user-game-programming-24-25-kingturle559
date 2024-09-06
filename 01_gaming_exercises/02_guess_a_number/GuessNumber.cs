9b414ab5633856c335dffc5e5d265ac4d74f91f9
kingturle559 <s11580859@students.duvalschools.org>
2024-09-06 13:57:17 -0400

    ra

---
+++ 01_gaming_exercises/02_guess_a_number/GuessNumber.cs
@@ -0,0 +1,17 @@
+using System;
+class GuessNumber {
+  static void Main() {
+    int numGuess = 0;
+    int maxGuess = 5;
+
+    //Generate the secret number here.
+    //While loop
+      //Allow the user to guess the number.
+      //Compare the guess to the secret number.
+      //If match, print win, if > or <, give a hint
+      //+1 to numGuess
+      //check if numGuess > maxGuesses.
+      //If true, print a "loose game" message, if false guess again.
+      
+  }
+}
