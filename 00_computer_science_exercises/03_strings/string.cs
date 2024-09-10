f3c8bec0cd0765df093f1798aaef9256829695bd
kingturle559 <s11580859@students.duvalschools.org>
2024-09-10 14:51:42 -0400

    Update string.cs

--- 00_computer_science_exercises/03_strings/string.cs
+++ 00_computer_science_exercises/03_strings/string.cs
@@ -15,13 +15,38 @@ class Template {
 
   if(greeting.Length > goodbye.Length)
   {
-    Console.WriteLine("The greeting is longer than the goodbye.\n")
+    Console.WriteLine("The greeting is longer than the goodbye.\n");
   }
 
   //useful sting Methods
 
   Console.WriteLine(greeting.ToUpper());// Make entire string UPPERCASE
   Console.WriteLine(greeting.ToUpper());// Make entire string lowercase
+
+  //Sting Concatenation "con-cat-Uh-Nation"
+  string comboString = greeting + goodbye;
+  Console.WriteLine(comboString);
+
+  //String concatenation Method #2
+  string comboString2 = string.Concat(greeting, goodbye);
+  Console.WriteLine(comboString2);
+
+  //string interpolation -- substitution variables into strings.
+  string comboString3 = $"My greeting is {greeting} and my goodbye is {goodbye}.\n";
+  Console.WriteLine(comboString3);
+
+  // Substituting variables into strings method 2
+  Console.WriteLine("My greeeting is " + greeting + " and my goodbye is " + goodbye + "\n.");
+
+  // Accessing Parts of Strings
+  // index is a specific location of a character in a string
+  // ALL string indexes start at 0
+  Console.WriteLine(goodbye[0]); // Print the character at index0.
+  Console.WriteLine(goodbye[4]); // Replace x wiht what to print the 5th character?
+  // Where is it in my string?
+  Console.WriteLine(greeting.IndexOf("y"));
+
+  
 
 
   }
