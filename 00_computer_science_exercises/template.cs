ad18c4d2c05d59cf396c16a6a756d23c05b0ed88
kingturle559 <s11580859@students.duvalschools.org>
2024-09-16 14:50:40 -0400

    Update template.cs

--- 00_computer_science_exercises/template.cs
+++ 00_computer_science_exercises/template.cs
@@ -1,6 +1,33 @@
 using System;
 class Template {
   static void Main() {
+   String CPUchoice = "";
+   String userChoice = "";
+   String userName = "";
+   
+    Console.WriteLine("To start, please enter you name and press ENTER");
+    userName = Console.ReadLine();
+    Console.WriteLine("WELCOME TO ROCK, PAPER, SCISSORS " + userName + "!\n");
+    Console.WriteLine("In this game you will be playing against the CPU.\nRock beats scissors, scissors beats paper, paper beats rock and every time you win a maatch you will gain a point.\nFirst one to 5 wins the game!");
+    Console.WriteLine("Input ROCK, PAPER OR SCISSORS.");
+    userChoice = Console.ReadLine();
+
+    if (userChoice = CPUchoice)
+      {
+        Console.WriteLine("Try Again.\n");
+      }
+    if ()
+      {
+        PlayerScore++
+      }
+     if PlayerScore = 5 
+      {
+     console.WriteLine("congrats you a winner!!!\n")
+      }
   
+      if CPUScore = 5
+      {
+      Console.WriteLine("You lost nerd!!!\n")
+     }
   }
 }
